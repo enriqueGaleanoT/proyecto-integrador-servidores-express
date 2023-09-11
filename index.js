3ebefde3a4c59670cf8f99a27de891053fb4f59f
enriqueGaleanoT <enriquej999@gmail.com>
2023-09-10 19:12:03 -0600

    Server is listening and doing petitionsof task list

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const app = express();
 const HOST = "http://localhost";
 const PORT = 2000;
 
+const taskList = [
+    {
+        "id":"123456",
+        "isCompleted":false,
+        "description":"Walk the dog",
+    }
+];
+
+
+app.get("/", (req, res)=>{
+    res.status(200).send(taskList);
+});
+
 app.listen(PORT, ()=>{
     console.log(`${HOST}:${PORT}`);
 });
