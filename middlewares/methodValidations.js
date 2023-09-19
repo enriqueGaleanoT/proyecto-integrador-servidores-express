32b0ad4033103695421fdc3b97306e987cee00c9
enriqueGaleanoT <enriquej999@gmail.com>
2023-09-19 14:08:08 -0600

    I separated middlewareslogic into different file

---
+++ middlewares/methodValidations.js
@@ -0,0 +1,53 @@
+const validateMethodPOST = (req, res, next) =>{
+    const dataPOST = req.body;
+    const {id, description, isCompleted} = req.body;
+    if(Object.keys(req.body).length === 0){
+        console.log("entra aqui");
+        return res.status(404).send({error: "Dont leave the body empty"});
+    } else if (!id || !description || !isCompleted) {
+        return res.status(404).send({error: "You need the appropriate JSON attributes"});
+    }
+
+    if ((dataPOST.id === " ") || (dataPOST.id === "")) {
+        return res.status(404).send({error: "Attribute Id is not filled"});
+    }else if (dataPOST.isCompleted === " " || dataPOST.isCompleted === ""){
+        return res.status(404).send({error: "Attribute isCompleted is not filled"});
+    }else if(dataPOST.description === " " || dataPOST.description === ""){
+        return res.status(404).send({error: "Attribute description is not filled"});
+    }
+    console.log(id + " " + isCompleted +  " " +  description);
+    next();
+};
+const validateMethodPUT = (req, res, next) =>{
+    const dataPUT = req.body;
+    const {id, description, isCompleted} = req.body;
+
+    if (!id || !description || !isCompleted) {
+        return res.status(404).send({error: "You need the appropriate JSON attributes"});
+    }
+
+    if (Object.keys(req.body).length === 0) {
+        return res.status(404).send({error: "Dont leave the body empty"})
+    }else if (dataPUT.id === " " || dataPUT.id === "") {
+        return res.status(404).send({error: "Attribute Id is not filled"});
+    }else if (dataPUT.isCompleted === " " || dataPUT.isCompleted === ""){
+        return res.status(404).send({error: "Attribute isCompleted is not filled"});
+    }else if(dataPUT.description === " " || dataPUT.description === ""){
+        return res.status(404).send({error: "Attribute description is not filled"});
+    }
+    next();
+};
+
+const validarMetodos = (req, res, next)=>{
+    const metodos = ['GET', 'POST', 'PUT', 'DELETE'];
+    if (!metodos.includes(req.method)) {
+        return res.status(404).send({message: "This Method is not allowed"});
+    }
+    next();
+}
+
+module.exports ={ 
+    validateMethodPOST,
+    validateMethodPUT,
+    validarMetodos
+};
--- router/list-edit-router.js
+++ router/list-edit-router.js
@@ -2,58 +2,59 @@ const express = require("express");
 const listEditRouter = express.Router();
 const taskList = require("../data");
 listEditRouter.use(express.json());
+const middlewaresMethods = require("../middlewares/methodValidations");
 
-const validateMethodPOST = (req, res, next) =>{
-    const dataPOST = req.body;
-    const {id, description, isCompleted} = req.body;
-    if(Object.keys(req.body).length === 0){
-        console.log("entra aqui");
-        return res.status(404).send({error: "Dont leave the body empty"});
-    } else if (!id || !description || !isCompleted) {
-        return res.status(404).send({error: "You need the appropriate JSON attributes"});
-    }
+// const validateMethodPOST = (req, res, next) =>{
+//     const dataPOST = req.body;
+//     const {id, description, isCompleted} = req.body;
+//     if(Object.keys(req.body).length === 0){
+//         console.log("entra aqui");
+//         return res.status(404).send({error: "Dont leave the body empty"});
+//     } else if (!id || !description || !isCompleted) {
+//         return res.status(404).send({error: "You need the appropriate JSON attributes"});
+//     }
 
-    if ((dataPOST.id === " ") || (dataPOST.id === "")) {
-        return res.status(404).send({error: "Attribute Id is not filled"});
-    }else if (dataPOST.isCompleted === " " || dataPOST.isCompleted === ""){
-        return res.status(404).send({error: "Attribute isCompleted is not filled"});
-    }else if(dataPOST.description === " " || dataPOST.description === ""){
-        return res.status(404).send({error: "Attribute description is not filled"});
-    }
-    console.log(id + " " + isCompleted +  " " +  description);
-    next();
-};
+//     if ((dataPOST.id === " ") || (dataPOST.id === "")) {
+//         return res.status(404).send({error: "Attribute Id is not filled"});
+//     }else if (dataPOST.isCompleted === " " || dataPOST.isCompleted === ""){
+//         return res.status(404).send({error: "Attribute isCompleted is not filled"});
+//     }else if(dataPOST.description === " " || dataPOST.description === ""){
+//         return res.status(404).send({error: "Attribute description is not filled"});
+//     }
+//     console.log(id + " " + isCompleted +  " " +  description);
+//     next();
+// };
 
-const validateMethodPUT = (req, res, next) =>{
-    const dataPUT = req.body;
-    const {id, description, isCompleted} = req.body;
+// const validateMethodPUT = (req, res, next) =>{
+//     const dataPUT = req.body;
+//     const {id, description, isCompleted} = req.body;
 
-    if (!id || !description || !isCompleted) {
-        return res.status(404).send({error: "You need the appropriate JSON attributes"});
-    }
+//     if (!id || !description || !isCompleted) {
+//         return res.status(404).send({error: "You need the appropriate JSON attributes"});
+//     }
 
-    if (Object.keys(req.body).length === 0) {
-        return res.status(404).send({error: "Dont leave the body empty"})
-    }else if (dataPUT.id === " " || dataPUT.id === "") {
-        return res.status(404).send({error: "Attribute Id is not filled"});
-    }else if (dataPUT.isCompleted === " " || dataPUT.isCompleted === ""){
-        return res.status(404).send({error: "Attribute isCompleted is not filled"});
-    }else if(dataPUT.description === " " || dataPUT.description === ""){
-        return res.status(404).send({error: "Attribute description is not filled"});
-    }
-    next();
-};
+//     if (Object.keys(req.body).length === 0) {
+//         return res.status(404).send({error: "Dont leave the body empty"})
+//     }else if (dataPUT.id === " " || dataPUT.id === "") {
+//         return res.status(404).send({error: "Attribute Id is not filled"});
+//     }else if (dataPUT.isCompleted === " " || dataPUT.isCompleted === ""){
+//         return res.status(404).send({error: "Attribute isCompleted is not filled"});
+//     }else if(dataPUT.description === " " || dataPUT.description === ""){
+//         return res.status(404).send({error: "Attribute description is not filled"});
+//     }
+//     next();
+// };
 
-const validarMetodos = (req, res, next)=>{
-    const metodos = ['GET', 'POST', 'PUT', 'DELETE'];
-    if (!metodos.includes(req.method)) {
-        return res.status(404).send({message: "This Method is not allowed"});
-    }
-    next();
-}
+// const validarMetodos = (req, res, next)=>{
+//     const metodos = ['GET', 'POST', 'PUT', 'DELETE'];
+//     if (!metodos.includes(req.method)) {
+//         return res.status(404).send({message: "This Method is not allowed"});
+//     }
+//     next();
+// }
 
 
-listEditRouter.use(validarMetodos);
+listEditRouter.use(middlewaresMethods.validarMetodos);
 
 
 
@@ -62,14 +63,14 @@ listEditRouter.get("/task",  (req, res)=>{
 });
 
 
-listEditRouter.post("/add-task", validateMethodPOST,(req, res)=>{
+listEditRouter.post("/add-task", middlewaresMethods.validateMethodPOST,(req, res)=>{
     const createTask = req.body;
     console.log(createTask);
     taskList.tasks.push(createTask);
     res.status(200).send({message: "Task added"});
 });
 
-listEditRouter.put("/update-task/:id", validateMethodPUT, (req, res)=>{
+listEditRouter.put("/update-task/:id", middlewaresMethods.validateMethodPUT, (req, res)=>{
     const taskId = req.params.id
     const taskFindId = taskList.tasks.findIndex((element) => element.id === taskId);
     console.log(taskFindId);
